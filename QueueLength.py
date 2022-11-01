098ffebf70d7ef9ba88812515fbb9826c4589964
KartikeyaSharma <kartikeya.rahul.sharma@gmail.com>
2022-11-01 22:27:07 +0530

    Cleaned QueueLength.py

--- QueueLength.py
+++ QueueLength.py
@@ -1,23 +1,11 @@
 import traci
 
-def getQueueLength(lane, length_threshold=200):
-	# num = 0
-	# lane_length = traci.lane.getLength(lane)
-
-	# for k in traci.lane.getLastStepVehicleIDs(lane):
-	# 	if traci.vehicle.getLanePosition(k) > lane_length - length_threshold:
-	# 		num += 1
-
-	# return num
-    return traci.lane.getLastStepHaltingNumber(lane)
-
-
-def getAllQueueLengths(length_threshold=200):
+def getAllQueueLengths():
     lanes = traci.lane.getIDList() # Returns a list of all lane IDs in the network
     total_queue_length = 0
 
     for lane in lanes:
-        queue_length = getQueueLength(lane)
+        queue_length = traci.lane.getLastStepHaltingNumber(lane) # Gives the number of vehicles travelling at speeds < 0.1 m/s in the lane
         total_queue_length += queue_length
 
     return total_queue_length

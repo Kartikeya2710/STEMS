a86c42c125d66de9ae90639dd5e6e7a1577e5963
KartikeyaSharma <kartikeya.rahul.sharma@gmail.com>
2022-11-05 13:14:06 +0530

    Refactored average delay function and main.py and updated TTL Performance image

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import traci
 import matplotlib.pyplot as plt
 from QueueLength import getAllQueueLengths
 from Throughput import get_throughput
+from CumulativeDelay import cummulative_delay_per_lane
 
 if 'SUMO_HOME' in os.environ:
 	tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
@@ -15,32 +16,46 @@ sumoBinary = "C:\\Program Files (x86)\\Eclipse\\Sumo\\bin\\sumo-gui.exe"
 sumoCmd = [sumoBinary, "-c", "simple.sumocfg"]
 NUM_STEPS = 1000
 
-
 traci.start(sumoCmd)
 step = 0
 queue_lens = []
 total_throughput = 0
 throughputs = []
 time_step = []
+avg_delays = []
 
 while step < NUM_STEPS:
 	traci.simulationStep() # Executes the next simulation step
+
 	queue_len = getAllQueueLengths() # Sum total of all the queue lengths in our network
 	queue_lens.append(queue_len)
-	time_step.append(step)	
+
 	total_throughput += get_throughput()
 	throughputs.append(total_throughput)
+
+	avg_delay = cummulative_delay_per_lane()
+	avg_delays.append(avg_delay)
+
+	time_step.append(step)
+
 	step += 1
 
 traci.close(False)
 
-fig, ax = plt.subplots(2, 1)
+
+# Plotting the metrics
+fig, ax = plt.subplots(3, 1)
 
 ax[0].plot(time_step, queue_lens, label="TTL")
-ax[0].set_title(f"Queue Length v/s {NUM_STEPS} SUMO Simulation Steps")
+ax[0].set_ylabel("Queue Length (No.)")
 
 ax[1].plot(time_step, throughputs, label="TTL")
-ax[1].set_title(f"Throughput v/s {NUM_STEPS} SUMO Simulation Steps")
+ax[1].set_ylabel("Throughtput (No.)")
+
+ax[2].plot(time_step, avg_delays, label="TTL")
+ax[2].set_ylabel("Lane Delay (sec)")
+
+plt.xlabel("Steps")
 
 plt.show()
 
